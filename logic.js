e6dcc841b0f6d63bbdc541b1ecb9244e0015f43c
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-11-10 19:43:46 +0100

    .

--- logic.js
+++ logic.js
@@ -2,16 +2,17 @@ m = 10
 n = 15
 k = 20
 end = false
+const ih = (i) => document.getElementById(`x${i}`)
 var l = Array(k).fill(" * ").concat(Array(m * n - k).fill(" - "))
 l.sort((a, b) => Math.random() - .5).sort((a, b) => Math.random() - .5)
 const g = (i, event) => {
-  if (document.getElementById(`x${i}`).innerHTML == "&nbsp; &nbsp;") {
-    document.getElementById(`x${i}`).innerHTML = "📍"
-    document.getElementById(`x${i}`).style.backgroundColor="rgb(255,170,184)"
+  if (ih(i).innerHTML == "&nbsp; &nbsp;") {
+    ih(i).innerHTML = "📍"
+    ih(i).style.backgroundColor="rgb(255,170,184)"
   }
   else {
-    document.getElementById(`x${i}`).innerHTML = "&nbsp; &nbsp;"
-    document.getElementById(`x${i}`).style.backgroundColor="aqua"
+    ih(i).innerHTML = "&nbsp; &nbsp;"
+    ih(i).style.backgroundColor="aqua"
   }
   event.preventDefault()
   return false
@@ -19,11 +20,11 @@ const g = (i, event) => {
 const f = i => {
   if (end) return true
   if (l[i][1] == "*") {
-    document.getElementById(`x${i}`).innerHTML = "💣"
-    document.getElementById(`x${i}`).style.backgroundColor="red"
+    ih(i).innerHTML = "💣"
+    ih(i).style.backgroundColor="red"
     end = true
     l.forEach((v, i) => { 
-      if (v[1] == "*") document.getElementById(`x${i}`).innerHTML = "💣"
+      if (v[1] == "*") ih(i).innerHTML = "💣"
     })
   }
   else {
@@ -34,8 +35,8 @@ const f = i => {
     szl.forEach(v => {
       if (i + v >= 0 && i + v < n * m && l[i + v][1] == "*") kosz++
     })
-    document.getElementById(`x${i}`).innerHTML = kosz
-    document.getElementById(`x${i}`).style.backgroundColor = `rgb(${120 + kosz * 30},${250 - kosz * 30},134)`
+    ih(i).innerHTML = kosz
+    ih(i).style.backgroundColor = `rgb(${120 + kosz * 30},${250 - kosz * 30},134)`
     l[i] = kosz + "-" + l[i][2]
     if (kosz == 0)
     szl.forEach(v => {
@@ -44,6 +45,9 @@ const f = i => {
   }
 }
 const main = () => {
-  document.getElementById("cont").innerHTML = l.map((v, i) =>
-    `<div id="x${i}" onclick="f(${i})" oncontextmenu="g(${i}, event)">&nbsp;${v[0]}&nbsp;</div>`).join("")
+  ih('c').innerHTML = l.map((v, i) => `
+  <div  id="x${i}" 
+        onclick="f(${i})" 
+        oncontextmenu="g(${i}, event)"
+  >&nbsp;${v[0]}&nbsp;</div>`).join("")
 }
