0f88d92510bedd4ed5fc46667f8105d2a31eb819
Dr. Németh Tamás <tomuwhu@gmail.com>
2023-11-17 08:42:29 +0100

    .

--- logic.js
+++ logic.js
@@ -1,18 +1,27 @@
-var [m, n, k, end, o] = [12, 17, 30, false, "&nbsp; &nbsp;"]
+var [m, n, k, q, q2, end, o] = [12, 17, 30, 0, 0, 0, "&nbsp; &nbsp;"]
 const ih = i => document.getElementById(`x${i}`)
 var l = Array(k)
         .fill(" * ").concat(Array(m * n - k).fill(" - "))
         .sort((a, b) => Math.random() - .5).sort((a, b) => Math.random() - .5)
 const g = (i, e) => {
+  e.preventDefault()
+  if (end) return true
   if (ih(i).innerHTML == o) {
     ih(i).innerHTML = "📍"
-    ih(i).style.backgroundColor="rgb(255,170,184)"
+    ih(i).style.backgroundColor = "rgb(255,170,184)"
+    q2++
+    if (l[i][1] == "*") q++
   }
   else {
     ih(i).innerHTML = o
-    ih(i).style.backgroundColor="aqua"
+    ih(i).style.backgroundColor = "aqua"
+    q2--
+    if (l[i][1] == "*") q--
   }
-  e.preventDefault()
+  if (q == k && q == q2) {
+    end = 2
+    document.getElementById(`ny`).innerHTML="Nyert!"
+  }
 }
 const f = i => {
   if (end) return true
@@ -20,10 +29,11 @@ const f = i => {
     ih(i).innerHTML = "💣"
     ih(i).style.backgroundColor="red"
     end = true
+    document.getElementById(`ny`).innerHTML="Veszített!"
     l.forEach((v, i) => ih(i).innerHTML = v[1] == "*" ?  "💣" : ih(i).innerHTML)
   } else {
     kosz = 0
-    szl = i % n == 0  ? [-n, -n + 1, 1, n, n + 1] :
+    szl = i % n == 1 - 1 ? [-n, -n + 1, 1, n, n + 1] :
           i % n == n - 1 ? [-n - 1, -n, -1, n - 1, n] :
           [-n - 1, -n, -n + 1, -1, 1, n - 1, n, n + 1]
     szl.forEach(v => 
